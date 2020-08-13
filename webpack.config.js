78f8025f0c29d742565b46494d689851abfd7b28
Akshay <akshayadimoolam16@gmail.com>
2020-08-13 15:37:44 +0530

    commiting a few changes

---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+const path = require('path');
+const postCSSPlugins = [
+    require('postcss-import'),
+    require('postcss-mixins'),
+    require('postcss-simple-vars'),
+    require('postcss-nested'),
+    require('autoprefixer')
+]
+
+module.exports = {
+    entry: './app/assets/scripts/App.js',
+    output: {
+        filename: 'bundles.js',
+        path: path.resolve(__dirname, 'app')
+    },
+    devServer: {
+        contentBase: path.join(__dirname, 'app'),
+        hot: true,
+        port: 3000,
+        host: '0.0.0.0',
+        before: function(app, server) {
+            server._watch('./app/**/*.html')
+        },
+    },
+    mode: 'production',
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader', {loader: 'postcss-loader', options: {plugins: postCSSPlugins}}]
+            }
+        ]
+    }
+}
