67e8b0f3f206b92dc5533710f6da3f3a19ac0b45
Akshay <akshayadimoolam16@gmail.com>
2020-08-13 22:22:21 +0530

    Example JavaScript

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -3,3 +3,7 @@ import '../styles/styles.css'
 if(module.hot) {
     module.hot.accept()
 }
+
+/*Lesson example code below this line*/
+console.log("Hello, my name is John Doe and my favourite color is blue.");
+console.log("Hello, my name is Jane Smith and my favourite color is green.");
