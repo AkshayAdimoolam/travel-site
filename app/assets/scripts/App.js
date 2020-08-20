ceeff9ff536511e5bef005507c239d76f1b0f8a2
Akshay <akshayadimoolam16@gmail.com>
2020-08-20 20:29:59 +0530

    Completed Desktop and Mobile header styles

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,9 +1,9 @@
 import '../styles/styles.css'
+import MobileMenu from './modules/MobileMenu'
+
+let mobileMenu = new MobileMenu();
 
 if(module.hot) {
     module.hot.accept()
 }
 
-/*Lesson example code below this line*/
-console.log("Hello, my name is John Doe and my favourite color is blue.");
-console.log("Hello, my name is Jane Smith and my favourite color is green.");
